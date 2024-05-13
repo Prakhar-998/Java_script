e4d9def68a08931cf9251d91d4be27ac6a8ba473
Prakhar-998 <143153505+Prakhar-998@users.noreply.github.com>
2024-05-13 17:16:57 +0000

    datatypes

---
+++ basic/02DataTypes.js
@@ -0,0 +1,26 @@
+"use Strict" //this will treat all the code according to new version
+
+// alert (3+3) this will work on browser console in inspect, but not using nodejs
+
+console.log(3 +       
+    3) // this will work but plz dont use
+
+console.log ("Prakhar")
+
+let name="Prakhar"
+let age="21"
+let isactive= true
+
+/*
+Primitive Data Types
+int,bigint,bool=>true/false,string=>""
+null=> standalone value ie empty
+undefined =>
+symbol=> unique
+*/
+// object
+
+console.log(typeof name);
+console.log(typeof "Prakhar")
+console.log(typeof undefined);// gives undefined
+console.log(typeof null); // interestingly it gives object 
