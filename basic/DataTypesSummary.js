d017a85ba3fae5f1425d9b398c7cc321608daeed
Prakhar-998 <143153505+Prakhar-998@users.noreply.github.com>
2024-05-20 11:50:53 +0000

    datatypessummary

---
+++ basic/DataTypesSummary.js
@@ -0,0 +1,40 @@
+//Primitive (call by value)
+/* 7 types:
+String,
+Number,
+Boolean,
+null,
+undefined
+Symbol
+BigInt
+*/
+
+//Reference(non primitive)
+/*Array
+Object
+Function
+*/
+
+//Dynamically typed language, ex u dont have to define type whlie taking variable with value 100
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId)
+
+const heroes=["a","b","c"]
+
+let myObj={
+    name: "Prakhar",
+    age: 21,
+}
+
+const myFunction=function(){
+    console.log('Hello')
+}
+
+//?console.log(myFunction)
+
+//check data types of function,null,undefined,array,object etc
+
+console.log(typeof myFunction)
