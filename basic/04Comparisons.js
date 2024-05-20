503d4618f71ff866c249e05deaa4b7ef884f5a74
Prakhar-998 <143153505+Prakhar-998@users.noreply.github.com>
2024-05-20 11:35:52 +0000

    comparison

---
+++ basic/04Comparisons.js
@@ -0,0 +1,13 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//comparison operatos(><=) treat null as 0, while == checks for exact same values to be true
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+//StrictCheck
+console.log("2"===2)
+console.log("2"==2)
