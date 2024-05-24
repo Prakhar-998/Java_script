b17d635aa09a510e6bf443abc078bed75fd4d0c8
Prakhar-998 <143153505+Prakhar-998@users.noreply.github.com>
2024-05-24 13:15:00 +0000

    fx2

--- 03Functions/Functions.js
+++ 03Functions/Functions.js
@@ -27,3 +27,38 @@ function loggedIn(userName){
 }
 console.log(loggedIn("Prakhar"))
 console.log(loggedIn())// if argument not given the undefined 
+
+//*************************************//
+
+function cartPrice(...num1){//rest operator ,pack items together
+    return num1
+}
+console.log(cartPrice(2,4,6))//prints [2,4,6]
+
+function cartPrice1(val1,val2,...num1){//rest operator ,pack items together
+    return num1
+}
+console.log(cartPrice1(2,4,6,8,10))//2 and 4 become val1 ,val2
+
+
+const user= {
+    name: "cap",
+    price: 80
+}
+function handleObject(anyObject){
+    console.log(`name is ${anyObject.name} and price is ${anyObject.price}`)
+}
+handleObject(user)
+//another way
+handleObject({
+    name: "hat",
+    price: 800
+})
+
+const getArray=[2,4,6,8,5]
+function returnAVal(getArray){
+    return `Second Value is ${getArray[1]}`
+}
+console.log(returnAVal(getArray))
+//similarly another way
+console.log(returnAVal([3,4,5,6,7]))
